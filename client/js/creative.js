2198491ddaa02a76c7f99c1fdef8a35aa8799a94
jessefranklin <jessefrankling@gmail.com>
2017-01-24 17:34:13 -0500

    close bootstrap dropdown when state is changed

--- client/js/creative.js
+++ client/js/creative.js
@@ -1,6 +1,10 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    
+    $('body').click(function(){
+		if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
+            $('.navbar-collapse').collapse('toggle');
+        }
+    });
 
 })(jQuery); // End of use strict
