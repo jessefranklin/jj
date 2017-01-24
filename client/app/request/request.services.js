4482470b659c67895fb685438a5ddb7be1f7a8df
jessefranklin <jessefrankling@gmail.com>
2017-01-24 17:33:18 -0500

    update user and job data

--- client/app/request/request.services.js
+++ client/app/request/request.services.js
@@ -2,13 +2,13 @@
 
   angular
     .module('app.profile')
-    .factory('requestService', ['$http','$q','requestRestService','$location', requestService]);
+    .factory('requestService', ['$http','$q','requestRestService','setuserService','$location','$document', requestService]);
 
-  requestService.$inject = ['$http','$q','requestRestService','$location'];
-  function requestService ($http,$q,requestRestService,$location) {
+  requestService.$inject = ['$http','$q','requestRestService','setuserService','$location','$document'];
+  function requestService ($http,$q,requestRestService,setuserService,$location,$document) {
     var vm = this;
-    vm.request = {};
     var requestData = [];
+    vm.submitted = false;
 
     createRequest=function(job,user,request){
       vm.request = {
@@ -22,16 +22,38 @@
           title: job.title,
           completed: false,
           archived: false,
+          active: false,
           provider_date:request.provider_date,
           provider_time:request.provider_time,
+          original_cost:job.cost.total_amount,
           bid:request.bid
       };
 
+      vm.requestArray = {
+        request_id:job._id,
+        title:job.title
+      };
+      vm.user = user;
       requestData.push(vm.request);
-      dialog.close();
+
+      //FIX FOR MODAL
+      angular.element($document[0].getElementsByClassName('modal-backdrop')).remove();
+      angular.element(document.body).removeClass('modal-open');
+
+      $location.path('/request');
+    };
+
+    submitRequest = function(){
+      vm.request.active = true;
       requestRestService.create(vm.request)
         .then(function(data){
-          $location.path('/request');
+          setuserService.addToUser(vm.user,vm.requestArray,'requests');
+          var applicant = {
+            applicant_id: vm.request.provider_id,
+            status: 'pending'
+          };
+          requestRestService.addToJob(vm.request.job_id,applicant);
+          vm.submitted = true;
         });
     };
 
@@ -41,6 +63,7 @@
 
     return {
       createRequest: createRequest,
+      submitRequest: submitRequest,
       getRequest: getRequest,
       close: close
     };
